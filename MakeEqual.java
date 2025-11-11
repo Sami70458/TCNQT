d1e6e0aa5483a1980523636f83b922bef5a51019
Samiksha Bharti <115936670+Sami70458@users.noreply.github.com>
2025-11-12 00:50:57 +0530

    Change array values in MakeEqual.java

--- MakeEqual.java
+++ MakeEqual.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class MakeEqual{
     public static void main(String[] args){
-        int[] arr = {3,3,3};
+        int[] arr = {1,3,5};
 
         int result = moves(arr);
 
